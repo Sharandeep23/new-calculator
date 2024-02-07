ecdd65d5702011f5c63122d751e3e3fdf554da38
Sharandeep <sharandeep23@yahoo.com>
2024-02-07 16:01:44 +0600

    minor js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const screen = document.querySelector('.screen');
 const buttons = document.querySelector('.calc-buttons');
+
 let screenValue = '0';
 let operator;
 let previousValue = 0;
@@ -10,40 +11,75 @@ buttons.addEventListener('click', function (e) {
 });
 
 function buttonClick(value) {
-  // For the numbers
-  if (parseInt(value) || parseInt(value) === 0) {
-    displayNumber(value);
+  if (isNaN(value)) {
+    // For symbols
+    handleSymbol(value);
+  } else {
+    // For numbers
+    handleNumber(value);
   }
-  // For other stuffs
-  else {
-    switch (value) {
-      case 'C':
-        clear();
-        break;
-      case '←':
-        backspace();
-        break;
-      case '÷':
-      case '×':
-      case '−':
-      case '+':
-        operator = value;
-        break;
-      case '=':
-        let result = calculate(previousValue, operator, parseInt(screenValue));
-        // convert num to string
-        result = result.toString();
-        // Result is trimmed if it is bigger than MAX_DIGIT
-        if (result.length > MAX_DIGIT) {
-          result = result.slice(0, 13);
-        }
+}
 
-        screenValue = result;
-        displayValue(screenValue);
-        previousValue = 0;
-        operator = '';
-    }
+function handleNumber(number) {
+  if (screenValue === '0') {
+    screenValue = number;
+  } else if (screenValue.length === MAX_DIGIT) {
+    alert('Please use less digits!');
+    return;
+  } else if (operator && !previousValue) {
+    previousValue = parseInt(screenValue);
+    screenValue = number;
+  } else {
+    screenValue += number;
   }
+  displayValue(screenValue);
+}
+
+function handleSymbol(symbol) {
+  switch (symbol) {
+    case 'C':
+      clear();
+      break;
+    case '←':
+      backspace();
+      break;
+    case '÷':
+    case '×':
+    case '−':
+    case '+':
+      operator = symbol;
+      break;
+    case '=':
+      let result = calculate(previousValue, operator, parseInt(screenValue));
+      // convert num to string
+      result = result.toString();
+      // Result is trimmed if it is bigger than MAX_DIGIT
+      if (result.length > MAX_DIGIT) {
+        result = result.slice(0, 13);
+      }
+
+      screenValue = result;
+      displayValue(screenValue);
+      previousValue = 0;
+      operator = '';
+  }
+}
+
+function clear() {
+  screenValue = '0';
+  previousValue = 0;
+  operator = '';
+  displayValue(screenValue);
+}
+
+function backspace() {
+  if (screenValue === '0') return;
+  screenValue = screenValue.slice(0, -1);
+  // For empty strings
+  if (screenValue === '') {
+    screenValue = '0';
+  }
+  displayValue(screenValue);
 }
 
 function calculate(operand1, operator, operand2) {
@@ -62,35 +98,6 @@ function calculate(operand1, operator, operand2) {
   }
 }
 
-function displayNumber(value) {
-  if (screenValue === '0') {
-    screenValue = value;
-  } else if (screenValue.length === MAX_DIGIT) {
-    alert('Please use less digits!');
-    return;
-  } else if (operator && !previousValue) {
-    previousValue = parseInt(screenValue);
-    screenValue = value;
-  } else {
-    screenValue += value;
-  }
-  displayValue(screenValue);
-}
-
-function clear() {
-  screenValue = '0';
-  displayValue(screenValue);
-}
-
-function backspace() {
-  if (screenValue === '0') return;
-  screenValue = screenValue.slice(0, -1);
-  // For empty strings
-  if (screenValue === '') {
-    screenValue = '0';
-  }
-  displayValue(screenValue);
-}
 function displayValue(value) {
   screen.innerText = value;
 }
