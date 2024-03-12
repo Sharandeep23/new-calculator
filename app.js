80accb833feca622ec741a63bcf522c23302257f
Sharandeep <sharandeep23@yahoo.com>
2024-03-12 18:18:32 +0600

    js fixing

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const screen = document.querySelector('.screen');
 const buttons = document.querySelector('.calc-buttons');
 
 let screenValue = '0';
-let operator = '';
-let previousValue = 0;
+let previousOperator;
+let runningTotal = 0;
 const MAX_DIGIT = 15;
 let isEqualPressed = false;
 
@@ -30,10 +30,12 @@ function handleNumber(number) {
     screenValue = number;
   } else if (screenValue.length === MAX_DIGIT) {
     alert('Please use less digits!');
-  } else if (operator && !previousValue) {
-    previousValue = parseInt(screenValue);
+  } else if (previousOperator && !runningTotal) {
+    // After previous operator, screen value should be replaced
+    runningTotal = parseInt(screenValue);
     screenValue = number;
   } else if (isEqualPressed) {
+    // If '=' was pressed, screen value should be replaced
     screenValue = number;
     isEqualPressed = false;
   } else {
@@ -52,20 +54,28 @@ function handleSymbol(sym) {
     // default case will be applied for '+', '-', '×', '÷' and '='
     default:
       if (screenValue === '0') return;
+
+      // changing previousOperator
+      // If a user presses '+'/'-' then '×'/'÷'
+      if (previousOperator && !runningTotal) {
+        previousOperator = sym;
+        return;
+      }
+
       // If Operator exists then it should match with sym
       // e.g. evaluate '5 + 5' and '+', . Not for any other signs except '='
-      if (operator && operator !== sym && sym !== '=') return;
+      if (previousOperator && previousOperator !== sym && sym !== '=') return;
 
       // Filtering Equals
-      if (sym === '=' && !previousValue) return;
+      if (sym === '=' && !runningTotal) return;
 
-      if (previousValue && operator) {
+      if (runningTotal && previousOperator) {
         evaluate();
       }
       if (sym === '=') {
         isEqualPressed = true;
       } else {
-        operator = sym;
+        previousOperator = sym;
         isEqualPressed = false;
       }
   }
@@ -77,15 +87,27 @@ function clear() {
 }
 
 function reset() {
-  previousValue = 0;
-  operator = '';
+  runningTotal = 0;
+  previousOperator = '';
   isEqualPressed = false;
 }
 
 function evaluate() {
-  let result = calculate(previousValue, operator, parseInt(screenValue));
+  switch (previousOperator) {
+    case '+':
+      runningTotal += parseInt(screenValue);
+      break;
+    case '−':
+      runningTotal -= parseInt(screenValue);
+      break;
+    case '×':
+      runningTotal *= parseInt(screenValue);
+      break;
+    case '÷':
+      runningTotal /= parseInt(screenValue);
+  }
   // convert num to string
-  result = result.toString();
+  let result = runningTotal.toString();
   // Result is trimmed if it is bigger than the MAX_DIGIT
   if (result.length > MAX_DIGIT) {
     result = result.slice(0, 13);
@@ -101,18 +123,5 @@ function backspace() {
     reset();
   } else {
     screenValue = screenValue.slice(0, -1);
-  }
-}
-
-function calculate(operand1, operator, operand2) {
-  switch (operator) {
-    case '÷':
-      return operand1 / operand2;
-    case '×':
-      return operand1 * operand2;
-    case '−':
-      return operand1 - operand2;
-    case '+':
-      return operand1 + operand2;
   }
 }
